c8125a0eeb1c347da31d0f434ee3296c975b31bd
ederaleng <36174193+ederaleng@users.noreply.github.com>
2024-07-07 15:58:29 -0500

    adding op id in transaction (#36)

--- src/services/bridge.js
+++ src/services/bridge.js
@@ -4,8 +4,8 @@ class Bridge extends JSONRequest {
   constructor() {
     super({ url: import.meta.env.VITE_NODE_BRIDGE || "https://proxy.vortexbridge.io" });
   }
-  getKoinTx(_tx) {
-    return this.getPrepared("/GetKoinosTransaction", { TransactionId: _tx })
+  getKoinTx(_tx, _opId) {
+    return this.getPrepared("/GetKoinosTransaction", { TransactionId: _tx, OpId: _opId })
   }
   getEthTx(_tx) {
     return this.getPrepared("/GetEthereumTransaction", { TransactionId: _tx })
